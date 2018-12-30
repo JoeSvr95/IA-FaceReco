427d5d76c16b429987c8c72f70f5ff9fe6a517e9
Joe <joesaverio@hotmail.com>
2018-12-30 03:24:26 -0500

    Uso del API face_recognition y dlibs

---
+++ src/encode_faces.py
@@ -0,0 +1,56 @@
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+# Directorio donde se ubica ESTE archivo
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "dataset")
+
+# Parámetro para poder elegir entre 'hog' o 'cnn'
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--detection-method", type=str, default="hog",
+    help="modelo de detección de caras a usar: 'hog' o 'cnn'")
+args = vars(ap.parse_args())
+
+# Recoger los paths de las imágenes
+print("[INFO] quantificando caras...")
+imagePaths = list(paths.list_images(image_dir))
+
+# Inicializando la lista de los encodings conocidos y nombres
+knownEncodings = []
+knownNames = []
+
+# Iteranmos las rutas de las imágenes
+for (i, imagePath) in enumerate(imagePaths):
+    # Extraemos el nombre de la persona de la ruta
+    print("[INFO] procesando imagen {}/{}".format(i + 1, len(imagePaths)))
+    name = imagePath.split(os.path.sep)[-2]
+
+    # Cargar la imagen input y convertira de BGR a dlib RGB
+    image = cv2.imread(imagePath)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Detectar las coordenadas (x, y) de los bounding boxes
+    # que corresponden a cada una de las caras de las imágenes
+    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+
+    # Computar las facciones de las caras
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    # Iterar los encodings
+    for encoding in encodings:
+        # Agregar cada encoding más el nombre a nuestro set
+        # de caras conocidas y encodings
+        knownEncodings.append(encoding)
+        knownNames.append(name)
+
+# Guardando los encodings de las caras más los nombres
+print("[INFO] serializando los encodings...")
+data = {"encodings": knownEncodings, "names": knownNames}
+
+f = open("encodings.pickle", 'wb')
+f.write(pickle.dumps(data))
+f.close
---
+++ src/recognize_faces_video.py
@@ -0,0 +1,84 @@
+from imutils.video import VideoStream
+import face_recognition
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+
+# Contruyendo los argumentos
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--detection_method", type=str, default="hog",
+    help="modelo de detección de rostro: 'hog' o 'cnn'")
+args = vars(ap.parse_args())
+
+# Cargar las caras reconocidas
+print("[INFO] cargando encodings...")
+data = pickle.loads(open("encodings.pickle", 'rb').read())
+
+# Inicializando la camara
+print("[INFO] iniciando camara...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+while True:
+    # Capturando los frames de la cámara
+    frame = vs.read()
+
+    # Convertir el frame de BGR a RGB y redimencionar para
+    # que tenga anchura de 750 px
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = imutils.resize(frame, width=750)
+    r = frame.shape[1] / float(rgb.shape[1])
+
+    # Detectando las coordenadas (x, y) de las cajas delimitadoras
+    # que corresponden a cada una de las caras del frame
+    boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    names = []
+
+    # Iterando sobre las caras
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+        name = "Desconocido"
+
+        # Verificando si encontramos un match
+        if True in matches:
+            # Encontrando todos los indices de las caras encontradas
+            # e inicializar un diccionario que cuente el número total de matches
+            matchedIndxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+
+            # Iterar sobre los indices que hicieron match y mantener un contador
+            # por cada cara reconocida
+            for i in matchedIndxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
+
+            # Determinar la cara reconocida con la mayor cantidad de votos
+            name = max(counts, key=counts.get)
+
+        # Actualizando la lista de nombres
+        names.append(name)
+
+    # Iterando sobre las caras reconocidas
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        # Re-escalar las coordenadas de la cara
+        top = int(top * r)
+        right = int(right * r)
+        bottom = int(bottom * r)
+        left = int(left * r)
+
+        # Colocar el nombre sobre las caras
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+            (0, 255, 0), 2)
+
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
